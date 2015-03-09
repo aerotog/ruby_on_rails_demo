e9f86527d0af36db2ba172cd867a79683f00b1ab
aerotog <aerotog@gmail.com>
2015-03-09 01:58:42 -0500

    Add gem faker for user seeding

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,22 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+User.create!(name:  "Example User",
+             email: "example@fakemail.org",
+             password:              "foobar",
+             password_confirmation: "foobar")
+
+User.create!(name:  "Timothy Guenthner",
+             email: "aerotog@gmail.com",
+             password:              "foobar",
+             password_confirmation: "foobar")
+
+9.times do |n|
+  name  = Faker::Name.name
+  email = "example-#{n+1}@railstutorial.org"
+  password = "password"
+  User.create!(name:  name,
+               email: email,
+               password:              password,
+               password_confirmation: password)
+end
